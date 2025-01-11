b3b21d90ff52493e368731e89e850bb95780e023
dbEsq <d_ber1@live.com>
2025-01-11 14:25:51 -0600

    Implement complete, exit button, d.17

--- gui.py
+++ gui.py
@@ -7,11 +7,18 @@ add_button = sg.Button('Add')
 list_box = sg.Listbox(values=functions.get_todos(),
                       key='todos',
                       enable_events=True,
-                      size=[45,10])
+                      size=[45,16])
+
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],  [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]
+                           ],
                    font=("Helvetica", 20))
 
 while True:
@@ -28,17 +35,27 @@ while True:
             window['todos'].update(values=todos)
         case 'Edit':
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
+            new_todo = values['todo']
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-        case 'todo':
+            window['todo'].update(value='')
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
+        case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
-
+print('bye')
 window.close()
 
 
